90aa5ce74f5b6d5b9cef289704b34031a61ffcde
euklid <ucabhem@ucl.ac.uk>
2017-05-01 22:22:04 +0100

    sped up angles code, fixed triangle split code

--- triangle_split.m
+++ triangle_split.m
@@ -1,40 +1,43 @@
 function [vo,fo] = triangle_split(v,f)
-
 %set angle thresh
 angle_thresh = 110*pi/180;
 angles = get_angles(v,f);
+idx = 1;
 
-for i = 1:size(f,1)
+while(idx <= size(f,1))
     %decide whether to split or not
-    triangle_split = false;
-    split_angle = false(3,1);
-    for j = 1:3
-        split_angle(j) = (angles(i,j)>=angle_thresh) ;
-        triangle_split = triangle_split || split_angle(j);
-    end
+    split_angle = double(angles(idx,:)>= angle_thresh);
+    triangle_split = (sum(split_angle) > 0);
     
     %split triangles
     if triangle_split
         %find the angle to be splited
-        col = find(split_angle>0);
+        col = sum([1 2 3].*split_angle);
         %take 3 vertices to derive the perpendicular point d
-        idx_v1 = f(i,col);
-        idx_v2 = f(i,rem(col,3)+1);
-        idx_v3 = f(i,rem(rem(col,3)+1,3)+1);
+        idx_v1 = f(idx,col);
+        idx_v2 = f(idx,rem(col,3)+1);
+        idx_v3 = f(idx,rem(rem(col,3)+1,3)+1);
+        toc;
         v1 = v(idx_v1,:);
         v2 = v(idx_v2,:);
         v3 = v(idx_v3,:);
         p = (v3 - v2)/norm(v3 - v2);
         q = (v1 - v2)/norm(v1 - v2);
-        tao = cross(p,cross(p,q));
-        tao = tao/norm(tao);
-        d = v1 + tao * dot(tao,(v2 - v1));
+        tau = cross(p,cross(p,q));
+        tau = tau/norm(tau);
+        d = v1 + tau * dot(tau,(v2 - v1));
         %adding new vertex and 2 new faces, delete the splited faces
         idx_d = size(v,1) + 1;
-        f(i,:) = [];
+        f(idx,:) = [];
         v(idx_d,:) = d;
-        f = [f ; idx_v1 idx_v2 idx_d ; idx_v1 idx_v3 idx_d];
-    end    
+        new_faces = [idx_v1 idx_v2 idx_d ; idx_v1 idx_v3 idx_d];
+        f = [f ; new_faces];
+        % update angle matrix
+        angles(idx,:) = [];
+        angles = [angles;get_angles(v,new_faces)];
+        idx = idx-1;
+    end
+    idx = idx+1;    
 end
 vo = v;
 fo = f;
